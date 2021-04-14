b5072b707813ef246c9a5cd2be1caa6ff58ebc39
Takumi Kanada <takumi.kanada@satori.team>
2021-04-14 06:42:30 +0000

    fix: tmux install.sh

--- tmux/install.sh
+++ tmux/install.sh
@@ -15,30 +15,26 @@ archivedir="tmux-${version}"
 
 prefix="/usr/local"
 
-if [ "$(uname -s)" != "Linux" ]; then
-  echo "Error: This script is only for Linux"
+if [ ! -e "/etc/debian_version" ]; then
+  echo "Error: This script is only for Debian"
   exit 1
 fi
 
-if [ -e "/etc/debian_version" ]; then
-  sudo apt install -y build-essential
-  sudo apt build-dep -y tmux
-fi
+apt install -y libevent-dev ncurses-dev build-essential bison pkg-config
 
 if [ ! -f "${tarball}" ]; then
   wget "https://github.com/tmux/tmux/releases/download/${version}/${tarball}" -O "${tarball}"
 fi
 
 if [ -e "${archivedir}" ]; then
-  sudo rm -rf "${archivedir}"
+  rm -rf "${archivedir}"
 fi
 
 tar zxvf "${tarball}"
 cd "${archivedir}"
 
-./configure --prefix=${prefix}
+./configure \
+  --prefix=${prefix} \
+  --enable-static
 make
 make install
-
-cd "${script_home}"
-sudo rm -rf "${archivedir}"
